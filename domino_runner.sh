535c4d99c749477bc4627efe4791258d962bf8bf
Nima Rahmanian <nimsi@berkeley.edu>
2021-06-21 19:28:14 +0000

    Remove extra active_gene_files/ directory in dedicated user directory.

--- domino_runner.sh
+++ domino_runner.sh
@@ -5,7 +5,12 @@ network_file=${2}
 output_folder=${3}
 python_env=${4}
 
-source ${python_env}/bin/activate; slicer --network_file ${network_file} --output_file ${network_file}".slicer" &&  domino  --active_genes_files ${active_genes_file} --network_file  ${network_file} --slices_file ${network_file}".slicer" --output_folder  ${output_folder} &>/dev/null
-# echo "source ${python_env}/bin/activate; slicer --network_file public/${network_file} --output_file public/${network_file}".slicer" &&  domino  --active_genes_files public/${active_genes_file} --network_file  public/${network_file} --slices_file public/${network_file}".slicer" --output_folder  ${output_folder}"
-# echo ${output_folder}/${active_genes_file%.*}/modules.out
-# cat ${output_folder}/${active_genes_file%.*}/modules.out
+# activate virtual environment
+source ${python_env}/bin/activate
+
+# run DOMINO preprocess and DOMINO
+slicer --network_file ${network_file} --output_file ${network_file}".slicer" &&  domino  --active_genes_files ${active_genes_file} --network_file  ${network_file} --slices_file ${network_file}".slicer" --output_folder  ${output_folder} &>/dev/null
+
+# set the directory of the modules static html files
+mv ${output_folder}/active_gene_files/* ${output_folder}
+rm -d ${output_folder}/active_gene_files
