f7fdabf2900c0f7e947c1ef4e18865f2e1389297
Nima Rahmanian <nimsi@berkeley.edu>
2021-09-02 08:17:05 +0300

    Catch bug in file_delete.sh

--- runners/file_delete.sh
+++ runners/file_delete.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd public
+file_to_delete_from=${1}
+
+cd file_to_delete_from
 find ./* -mmin +60 -type d \( ! -name "networks" \) | xargs -I {} rm -fr {}
 find ./* -mmin +60 -name "*.zip" | xargs -I {} rm -fr {}
