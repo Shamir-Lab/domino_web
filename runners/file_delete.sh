a041adc30833b677857ecc655be1f12e56ed4f83
Nima Rahmanian <69220570+Nimsi123@users.noreply.github.com>
2021-09-05 12:07:34 -0700

    Update file_delete.sh

--- runners/file_delete.sh
+++ runners/file_delete.sh
@@ -2,6 +2,6 @@
 
 file_to_delete_from=${1}
 
-cd file_to_delete_from
+cd ${file_to_delete_from}
 find ./* -mmin +60 -type d \( ! -name "networks" \) | xargs -I {} rm -fr {}
 find ./* -mmin +60 -name "*.zip" | xargs -I {} rm -fr {}
